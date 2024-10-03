7e572c14bcd452d13431e004a59c75947e437b00
Bode Favour <bodefavour@gmail.com>
2024-10-03 06:28:43 -0700

    create a script that returns a promise from a prototype function and the parameter is boolean

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,19 +1,2 @@
 function getResponseFromAPI() {
-return new Promise((resolve, reject) => {
-	const success = true;
-	if (success) {
-		resolve("API Call Succedd!");
-	}
-	else {
-		reject("API Call Rejected");
-	}
-});
-}
-
-getResponseFromAPI()
-.then((response) => {
-	console.log(response);
-}
-.catch((error) => {
-	console.log(error);
-}
+return new Promise(() => {});
