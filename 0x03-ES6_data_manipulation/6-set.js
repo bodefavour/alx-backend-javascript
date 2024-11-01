cbb9e86e80477a677fc26f6e1517eeb042d2226e
Favour Bode <bodefavour@gmail.com>
2024-11-01 16:17:54 +0100

    correct errors

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Bode Favour <https://github.com/bodefavour>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
